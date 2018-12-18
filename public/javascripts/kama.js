c36cde4fbdb5589bbe670929eaf9777b78fc82eb
intohimo1105 <intohimo1105@ukr.net>
2018-12-18 13:02:58 +0200

    super

--- public/javascripts/kama.js
+++ public/javascripts/kama.js
@@ -1,9 +1,9 @@
-var inputsTel = document.querySelectorAll('input[type="tel"]');
+// var inputsTel = document.querySelectorAll('input[type="text"]');
 
-Inputmask({
-  "mask": "+38(999) 999-99-99",
-  showMaskOnHover: false
-}).mask(inputsTel);
+// Inputmask({
+//   "mask": "+38(999) 999-99-99",
+//   showMaskOnHover: false
+// }).mask(inputsTel);
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
